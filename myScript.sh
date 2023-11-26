b485b5fc1e026392b43fd4e968a2cd242047b963
moDallel <115635659+moDallel@users.noreply.github.com>
2023-11-26 20:45:10 +0100

    add a script to automate deploying and installing dependencies

---
+++ myScript.sh
@@ -0,0 +1,20 @@
+cd Continuum/socialNetwork/
+git pull
+#git checkout test
+docker-compose up -d
+sudo apt-get install -y libssl-dev
+sudo apt-get install -y libz-dev
+sudo apt-get install -y luarocks
+sudo apt-get install -y liblua5.2-dev
+sudo luarocks install -y luasocket
+sudo pip3 install -y aiohttp
+sudo pip3 install -y asyncio
+sudo apt install -y nodejs
+sudo apt install -y npm
+python3 scripts/init_social_graph.py --graph=socfb-Reed98
+cd post_automation
+npm install
+npm run start
+cd ../
+#cd ../wrk2
+#make
